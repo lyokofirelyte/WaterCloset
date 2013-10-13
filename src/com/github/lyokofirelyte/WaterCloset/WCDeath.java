47e763a048f2d999630ae9c1061a301949aca2fe
WinneonSword <winneonsword@gmail.com>
2013-10-12 22:47:15 -0500

    Made death messages 100% <insert something here>

--- src/com/github/lyokofirelyte/WaterCloset/WCDeath.java
+++ src/com/github/lyokofirelyte/WaterCloset/WCDeath.java
@@ -62,19 +62,19 @@ public class WCDeath implements Listener{
 			switch (dc){
 			
 			default:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&odied. Why? I don't know or care. They're dead. DEAD. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fdied. Why? I don't know or care. They're dead. DEAD. &7(" + deaths + "&7)"));
 				break;
 
 			case BLOCK_EXPLOSION:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&oexploded. Like that block of TNT they were standing on. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fexploded. Like that block of TNT they were standing on. &7(" + deaths + "&7)"));
 				break;
 				
 			case FALL:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oTurns out that " + p.getDisplayName() + " &f&ocan't actually fly. Looks like that hurt. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fTurns out that " + p.getDisplayName() + " &fcan't actually fly. Looks like that hurt. &7(" + deaths + "&7)"));
 				break;
 				
 			case DROWNING:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&othought they were a fish. Now they're dead. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fthought they were a fish. Now they're dead. &7(" + deaths + "&7)"));
 				break;
 				
 			case ENTITY_ATTACK:
@@ -86,20 +86,23 @@ public class WCDeath implements Listener{
 						ItemStack weapon = ((HumanEntity) attacker).getItemInHand();
 						if (weapon.hasItemMeta() && weapon.getItemMeta().hasDisplayName()){
 							String weaponName = weapon.getItemMeta().getDisplayName();
-							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&otook on one too many " + attackerFinal + "&f&os, who used &b&o&l" + weaponName + "&f&o. &7(" + deaths + "&7)"));
+							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &b&o&l" + weaponName + "&f&o. &7(" + deaths + "&7)"));
 						} else {
-							String weaponName = weapon.getType().toString().toLowerCase().replaceAll("_", " ");
-							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&otook on one too many " + attackerFinal + "&f&os, who used &6" + weaponName + "&f&o. &7(" + deaths + "&7)"));
+							String weaponName = weapon.getType().name();
+							if (weaponName.equals("air")){
+								Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &6their fist &f. &7(" + deaths + "&7)"));
+							}
+							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &6" + weaponName + "&f. &7(" + deaths + "&7)"));
 						}
 						break;
 					} else {
-						EntityType attackerFinal = ((EntityDamageByEntityEvent) ede).getDamager().getType();
-						Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&otook on one too many " + attackerFinal + "&f&os, who used THUNDER SMASH!. &7(" + deaths + "&7)"));
+						String attackerFinal = ((EntityDamageByEntityEvent) ede).getDamager().getType().name();
+						Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "&fs, who used THUNDER SMASH!. &7(" + deaths + "&7)"));
 						break;
 					}
 				
 			case FALLING_BLOCK:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oThe sky is falling! The sk- wait, nope, was just a falling block. Poor " + p.getDisplayName() + "&f&o. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fThe sky is falling! The sk- wait, nope, was just a falling block. Poor " + p.getDisplayName() + "&f. &7(" + deaths + "&7)"));
 				break;
 			
 			case FIRE: case FIRE_TICK:
@@ -107,43 +110,43 @@ public class WCDeath implements Listener{
 				break;
 				
 			case LAVA:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oJump in the lava, they said. It'll be fine, they said. (" + p.getDisplayName() + "&f&o) &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fJump in the lava, they said. It'll be fine, they said. (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
 				break;
 				
 			case LIGHTNING:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oSo, " + p.getDisplayName() + "&f&o's lightning rod works. Really Well. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fSo, " + p.getDisplayName() + "&f's lightning rod works. Really Well. &7(" + deaths + "&7)"));
 				break;
 				
 			case MAGIC:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oMajyyk. (" + p.getDisplayName() + "&f&o) &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fMajyyk. (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
 				break;
 				
 			case MELTING:
-				Bukkit.broadcastMessage(WCMail.AS("&f&oMelting is a death event? (" + p.getDisplayName() + "&f&o) &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS("&fMelting is a death event? (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
 				break;
 				
 			case PROJECTILE:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&o was impailed by an arrow and didn't survive. Luckily, their knee is intact. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f was impailed by an arrow and didn't survive. Luckily, their knee is intact. &7(" + deaths + "&7)"));
 				break;
 				
 			case SUFFOCATION:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&osuffocated in a long, drawn-out process that was very painful. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fsuffocated in a long, drawn-out process that was very painful. &7(" + deaths + "&7)"));
 				break;
 				
 			case SUICIDE:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&owasted tons of cash on those suicide-prevention meetings. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fwasted tons of cash on those suicide-prevention meetings. &7(" + deaths + "&7)"));
 				break;
 				
 			case WITHER:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&owithered away. Get it? Withered? HAHAHAH.. ha..haa... a...just /suicide. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fwithered away. Get it? Withered? HAHAHAH.. ha..haa... a...just /suicide. &7(" + deaths + "&7)"));
 				break;
 				
 			case VOID:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&ofell out of the world. That's a thing. &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ffell out of the world. That's a thing. &7(" + deaths + "&7)"));
 				break;
 				
 			case CONTACT:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f&ojust wanted to give that cactus a hug! &7(" + deaths + "&7)"));
+				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fjust wanted to give that cactus a hug! &7(" + deaths + "&7)"));
 				break;
 			}
 		}
