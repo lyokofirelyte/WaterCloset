4fe89c0ee274d832b790737ff12c4190d1c22122
WinneonSword <winneonsword@gmail.com>
2013-10-13 15:55:06 -0500

    Redesigned the death message system

--- src/com/github/lyokofirelyte/WaterCloset/WCDeath.java
+++ src/com/github/lyokofirelyte/WaterCloset/WCDeath.java
@@ -1,10 +1,11 @@
 package com.github.lyokofirelyte.WaterCloset;
 
+import java.util.List;
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -53,107 +54,98 @@ public class WCDeath implements Listener{
 	
 		if (ent instanceof Player){
 			
-			Player p = (Player)ent;
+			Player p = (Player) ent;
 			int deaths = plugin.datacore.getInt("Users." + p.getName() + ".DeathCount");
 			deaths++;
 			plugin.datacore.set("Users." + p.getName() + ".DeathCount", deaths);
 			((PlayerDeathEvent) e).setDeathMessage(null);
-
-			switch (dc){
+			String message = null;
 			
-			default:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fdied. Why? I don't know or care. They're dead. DEAD. &7(" + deaths + "&7)"));
-				break;
-
-			case BLOCK_EXPLOSION:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fexploded. Like that block of TNT they were standing on. &7(" + deaths + "&7)"));
-				break;
+			if (dc == DamageCause.ENTITY_ATTACK){
 				
-			case FALL:
-				Bukkit.broadcastMessage(WCMail.AS("&fTurns out that " + p.getDisplayName() + " &fcan't actually fly. Looks like that hurt. &7(" + deaths + "&7)"));
-				break;
+				EntityDamageByEntityEvent edee = (EntityDamageByEntityEvent) ent;
+				message = tPD(p, dc, edee);
 				
-			case DROWNING:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fthought they were a fish. Now they're dead. &7(" + deaths + "&7)"));
-				break;
 				
-			case ENTITY_ATTACK:
-				Entity attacker = ((EntityDamageByEntityEvent) ede).getDamager();
+			} else {
 				
-					if (attacker instanceof Player){
-						
-						String attackerFinal = ((Player) attacker).getDisplayName();
-						ItemStack weapon = ((HumanEntity) attacker).getItemInHand();
-						if (weapon.hasItemMeta() && weapon.getItemMeta().hasDisplayName()){
-							String weaponName = weapon.getItemMeta().getDisplayName();
-							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &b&o&l" + weaponName + "&f&o. &7(" + deaths + "&7)"));
-						} else {
-							String weaponName = weapon.getType().name();
-							if (weaponName.equals("air")){
-								Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &6their fist &f. &7(" + deaths + "&7)"));
-							}
-							Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "'s, who used &6" + weaponName + "&f. &7(" + deaths + "&7)"));
-						}
-						break;
-					} else {
-						String attackerFinal = ((EntityDamageByEntityEvent) ede).getDamager().getType().name();
-						Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ftook on one too many " + attackerFinal + "&fs, who used THUNDER SMASH!. &7(" + deaths + "&7)"));
-						break;
-					}
+				message = tPD(p, dc);
 				
-			case FALLING_BLOCK:
-				Bukkit.broadcastMessage(WCMail.AS("&fThe sky is falling! The sk- wait, nope, was just a falling block. Poor " + p.getDisplayName() + "&f. &7(" + deaths + "&7)"));
-				break;
-			
-			case FIRE: case FIRE_TICK:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " is on fire! Kidding, they're dead. &7(" + deaths + "&7)"));
-				break;
-				
-			case LAVA:
-				Bukkit.broadcastMessage(WCMail.AS("&fJump in the lava, they said. It'll be fine, they said. (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
-				break;
-				
-			case LIGHTNING:
-				Bukkit.broadcastMessage(WCMail.AS("&fSo, " + p.getDisplayName() + "&f's lightning rod works. Really Well. &7(" + deaths + "&7)"));
-				break;
-				
-			case MAGIC:
-				Bukkit.broadcastMessage(WCMail.AS("&fMajyyk. (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
-				break;
-				
-			case MELTING:
-				Bukkit.broadcastMessage(WCMail.AS("&fMelting is a death event? (" + p.getDisplayName() + "&f) &7(" + deaths + "&7)"));
-				break;
-				
-			case PROJECTILE:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &f was impailed by an arrow and didn't survive. Luckily, their knee is intact. &7(" + deaths + "&7)"));
-				break;
-				
-			case SUFFOCATION:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fsuffocated in a long, drawn-out process that was very painful. &7(" + deaths + "&7)"));
-				break;
-				
-			case SUICIDE:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fwasted tons of cash on those suicide-prevention meetings. &7(" + deaths + "&7)"));
-				break;
-				
-			case WITHER:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fwithered away. Get it? Withered? HAHAHAH.. ha..haa... a...just /suicide. &7(" + deaths + "&7)"));
-				break;
-				
-			case VOID:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &ffell out of the world. That's a thing. &7(" + deaths + "&7)"));
-				break;
-				
-			case CONTACT:
-				Bukkit.broadcastMessage(WCMail.AS(p.getDisplayName() + " &fjust wanted to give that cactus a hug! &7(" + deaths + "&7)"));
-				break;
 			}
+			
+			Bukkit.broadcastMessage(message);
+			
 		}
-		  
 		
 	}
+	
+	public String tPD(Player p, DamageCause dc){
 		
+		Random rand = new Random();
+		List<String> dML = plugin.config.getStringList("Core.DeathMessages." + dc.toString());
+		int dMN = rand.nextInt(dML.size());
+		String message = dML.get(dMN);
 		
+		int deaths = plugin.datacore.getInt("Users." + p.getName() + ".DeathCount");
+		StringBuilder sb = new StringBuilder(message);
+		
+		sb.append(" &7( " + deaths + ")");
+		
+		message = "&o" + sb.toString();
+		message = WCMail.AS(message.replace("%p", p.getDisplayName() + "&r&o"));
+		
+		return message;
+		
+	}
+	
+	public String tPD(Player p, DamageCause dc, EntityDamageByEntityEvent ede){
+		
+		Random rand = new Random();
+		List<String> dML = plugin.config.getStringList("Core.DeathMessages." + dc.toString() + "." + ede.getDamager().getType().toString());
+		
+		if (dML == null){
+			
+			dML = plugin.config.getStringList("Core.DeathMessages." + dc.toString() + ".DEFAULT");
+			
+		}
+		
+		int dMN = rand.nextInt(dML.size());
+		String message = dML.get(dMN);
+		
+		int deaths = plugin.datacore.getInt("Users." + p.getName() + ".DeathCount");
+		StringBuilder sb = new StringBuilder(message);
+		
+		sb.append(" &7( " + deaths + ")");
+		
+		message = "&o" + sb.toString();
+		
+		if (ede.getDamager() instanceof Player){
+			
+			Player player = (Player) ede;
+			String pDN = player.getDisplayName();
+			ItemStack weapon = player.getItemInHand();
+			String wN = weapon.getItemMeta().getDisplayName();
+			
+			if (wN == null){
+				
+				wN = weapon.getType().name();
+				message = WCMail.AS(message.replace("%p", p.getDisplayName() + "&r&o").replace("%a", "&6" + pDN + "&r&o").replace("%i", "&6" + wN + "&r&o"));
+				
+			} else {
+				
+				message = WCMail.AS(message.replace("%p", p.getDisplayName() + "&r&o").replace("%a", "&6" + pDN + "&r&o").replace("%i", "&6" + wN + "&r&o"));
+				
+			}
+			
+		} else {
+			
+			String aTT = ede.getDamager().getType().name();
+			message = WCMail.AS(message.replace("%p", p.getDisplayName() + "&r&o").replace("%a", "&6" + aTT + "&r&o"));
+			
+		}
+		
+		return message;
+		
+	}
 	
 }
